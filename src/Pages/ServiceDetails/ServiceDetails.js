4131f1c8451b462a07002b3c504fd63e245b24aa
touhi13 <touhi13@gmail.com>
2021-10-20 23:57:19 +0600

    first commit

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
+
 
 const ServiceDetails = () => {
+    const { serviceId } = useParams();
+    const [service, setService] = useState({});
+    const history = useHistory();
+    console.log()
     return (
         <div>
             
---
+++ src/contexts/ServiceData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServiceData = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServiceData;
